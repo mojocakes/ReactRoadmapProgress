3abdec207f7d2958c788d7cb90828b07fcb9bcb1
James Cook <jamespetercook@googlemail.com>
2019-03-17 19:18:35 +0000

    - 📦 v1.0.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,15 @@
 const mix = require('laravel-mix');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-mix.ts('src/index.ts', 'build/js')
-    .ts('src/demo.tsx', 'build/js')
-    .sass('src/styles/index.scss', 'build/css')
-    .copy('src/demo.html', 'build/demo.html');
+if (process.env.NODE_ENV === 'production') {
+    mix.ts('src/index.ts', 'dist/js')
+        .sass('src/styles/index.scss', 'dist/css');
+} else {
+    mix.ts('src/index.ts', 'build/js')
+        .ts('src/demo.tsx', 'build/js')
+        .sass('src/styles/index.scss', 'build/css')
+        .copy('src/demo.html', 'build/demo.html');
+}
 
 /**
  * Webpack Configuration
